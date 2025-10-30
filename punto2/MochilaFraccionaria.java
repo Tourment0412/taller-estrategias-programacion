4041ce2f846379589ea57b4af40ee0ff5aa98e28
Santiago Quintero Uribe <santiquinterouribe0412@gmail.com>
2025-10-30 15:40:22 -0500

    Create MochilaFraccionaria.java

---
+++ punto2/MochilaFraccionaria.java
@@ -0,0 +1,54 @@
+package punto2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Objeto {
+    String nombre;
+    int valor;
+    int peso;
+
+    public Objeto(String nombre, int valor, int peso) {
+        this.nombre = nombre;
+        this.valor = valor;
+        this.peso = peso;
+    }
+
+    public double getValorPorPeso() {
+        return (double) valor / peso;
+    }
+    
+}
+
+public class MochilaFraccionaria {
+
+    public double obtenerMaximo(List<Objeto> objetos, int capacidadMochila) {
+        // Ordenar los objetos por valor/peso en orden descendente
+        objetos.sort((o1, o2) -> Double.compare(o2.getValorPorPeso(), o1.getValorPorPeso()));
+
+        double valorTotal = 0.0;
+        int w=capacidadMochila;
+        ArrayList<Objeto> objetosSeleccionados = new ArrayList<>();
+
+        for (Objeto obj : objetos) {
+            if (w == 0) {
+                break; // La mochila está llena
+            }
+            if (obj.peso <= w) {
+                // Si el objeto cabe completamente, lo añadimos
+                w -= obj.peso;
+                valorTotal += obj.valor;
+                objetosSeleccionados.add(obj);
+            } else {
+                // Si no cabe completamente, añadimos la fracción que cabe
+                double fraccion = (double) w / obj.peso;
+                valorTotal += obj.valor * fraccion;
+                objetosSeleccionados.add(new Objeto(obj.nombre, (int) (obj.valor * fraccion), (int) (obj.peso * fraccion))); // Añadir el objeto aunque sea fraccionado
+                w = 0; // La mochila está llena
+            }
+        }
+
+        return valorTotal;
+    }
+    
+}
